f1eab436ba7a7236c109b86750b519e06b333f3b
DaR3kDev <villegasdeveloper@outlook.es>
2026-04-12 10:31:19 -0500

    frist commit

---
+++ uni-chat-backend/Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace uni_chat_backend.Domain.Entities;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+
+    [BsonElement("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("email")]
+    public string Email { get; set; } = string.Empty;
+
+    [BsonElement("password")]
+    public string Password { get; set; } = string.Empty;
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
