bab6770017dae14e60dae12235eee3ae7209443c
DaR3kDev <villegasdeveloper@outlook.es>
2026-05-22 16:32:31 -0500

    style(DocsConfiguration): fix variable indentation

--- uni-chat-backend/API/Configuration/DependencyInjection/DocsConfiguration.cs
+++ uni-chat-backend/API/Configuration/DependencyInjection/DocsConfiguration.cs
@@ -6,7 +6,7 @@ public static class DocsConfiguration
 {
     public static void UseApiDocs(this WebApplication app)
     {
-         var enableDocs =
+        var enableDocs =
             app.Environment.IsDevelopment() ||
             Environment.GetEnvironmentVariable("ENABLE_DOCS") == "true";
 
