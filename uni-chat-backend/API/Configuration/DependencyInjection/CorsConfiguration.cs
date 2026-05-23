666c5ab2a8e56561433e2de139ea241c1cf7fc29
Kevin Villegas Pérez <villegasdeveloper@outlook.es>
2026-05-23 15:21:42 -0500

    Update CorsConfiguration.cs

--- uni-chat-backend/API/Configuration/DependencyInjection/CorsConfiguration.cs
+++ uni-chat-backend/API/Configuration/DependencyInjection/CorsConfiguration.cs
@@ -6,7 +6,8 @@ public static class CorsConfiguration
     [
         "http://localhost:5173",
         "http://localhost:3000",
-        "http://localhost:8080
+        "http://localhost:8080",
+        "http://172.24.239.24"
     ];
 
     public static void AddCorsConfiguration(this IServiceCollection services)
